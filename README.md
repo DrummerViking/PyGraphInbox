fe03ffe72fdc04cc9bf58c0c731b55da9fc96c14
Your Name <you@example.com>
2022-08-10 01:10:04 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PyGraphInbox
-Get EXO mail messages via Graph using Python
+
+Get EXO mail messages via Graph using Python.  
+Messages are pulled from the inbox of the users.  
+
+# Files
+
+| fileName | Description |
+|----------|-------------|
+|Inbox.py  | Main script file. Connect to each mailbox listed in the 'users.txt' file and output the messages to the console. |  
+|Requirements.txt | Requirements file listing required modules to be installed in order to run the script. |  
+| users.txt | Txt file with the list of primary smtp addresses of the mailboxes the script will work on. |
+| config.cfg | Config file. Required lines are:<br>**ClientID**<br>**TenantID**<br>**ClientSecret**<br>**authtenant**<br>**userslistfilename**<br> |
+- _Note that the last line of the config.cfg file, references the text file with the list of email addresses. You can modify this line to map to a different file name._  
+- _You need to have an Azure App registered. If you want to know how to create the app, you can follow these [steps.](https://docs.microsoft.com/en-us/graph/tutorials/python?tabs=aad&tutorial-step=7)_  
+
+# How to run
+
+In my testing I used Windows.  
+You can open Command Prompt or Powershell and run:  
+`Python.exe inbox.py`  
+it will read the config file, and lookup for the userslistfilename file to fetch the list of mailboxes.  
+<br>
+If the line 'userslistfilename' in the config file does not exists, you can pass the email addresses as arguments:  
+`Python.exe inbox.py "user1@contoso.com"`
+or even:  
+`Python.exe inbox.py "user1@contoso.com","user2@contoso.com"`
---
+++ inbox.py
@@ -0,0 +1,113 @@
+﻿import configparser
+import os
+import sys
+from azure.identity import (
+    InteractiveBrowserCredential,
+    ClientSecretCredential,
+    DeviceCodeCredential,
+)
+from configparser import SectionProxy
+from msgraph.core import GraphClient
+
+
+class Graph:
+    settings: SectionProxy
+    device_code_credential: DeviceCodeCredential
+    user_client: GraphClient
+    client_credential: ClientSecretCredential
+    app_client: GraphClient
+    username: str
+
+    def __init__(self, config: SectionProxy):
+        self.settings = config
+        client_id = self.settings["clientId"]
+        tenant_id = self.settings["tenantId"]
+        if "clientsecret" in list(self.settings.keys()):
+            print("Logging on with Application flow.")
+            secret = self.settings["clientSecret"]
+            graph_scopes = self.settings["graphappscopes"].split(" ")
+            self.client_credential = ClientSecretCredential(
+                tenant_id, client_id, secret
+            )
+            self.app_client = GraphClient(
+                credential=self.client_credential, scopes=graph_scopes
+            )
+        else:
+            print("Logging on with Delegation flow.")
+            graph_scopes = self.settings["graphuserscopes"].split(" ")
+            self.device_code_credential = DeviceCodeCredential(
+                client_id=client_id, tenant_id=tenant_id
+            )
+            self.user_client = GraphClient(
+                credential=self.device_code_credential, scopes=graph_scopes
+            )
+
+    def get_token(self):
+        if "clientsecret" in list(self.settings.keys()):
+            graph_scopes = self.settings["graphappscopes"]
+            access_token = self.client_credential.get_token(graph_scopes)
+        else:
+            graph_scopes = self.settings["graphuserscopes"]
+            access_token = self.device_code_credential.get_token(graph_scopes)
+        return access_token.token
+
+    def get_inbox(self, primarysmtpaddress):
+        endpoint = f"/users/{primarysmtpaddress}/mailfolders/inbox/messages"
+        # Only request specific properties
+        select = "from,isRead,receivedDateTime,subject"
+        # Get at most 25 results
+        top = 25
+        # Sort by received time, newest first
+        order_by = "receivedDateTime DESC"
+        request_url = f"{endpoint}?$select={select}&$top={top}&$orderBy={order_by}"
+        if "clientsecret" in list(self.settings.keys()):
+            inbox_response = self.app_client.get(request_url)
+        else:
+            inbox_response = self.user_client.get(request_url)
+        return inbox_response.json()
+
+
+if __name__ == "__main__":
+    # Load settings
+    config = configparser.ConfigParser()
+    this_file_path = os.path.abspath(__name__)
+    BASE_DIR = os.path.dirname(this_file_path)
+    config_file = os.path.join(BASE_DIR, "config.cfg")
+    if not os.path.exists(config_file):
+        print(f"{config_file} does not exists.")
+        raise FileNotFoundError
+    config.read(config_file)
+    azure_settings = config["Azure"]
+    # create graph object
+    graph: Graph = Graph(azure_settings)
+    # get email messages
+    # checking if users file list exists in config file
+    if "userslistfilename" in list(azure_settings.keys()):
+        users_file = os.path.join(BASE_DIR, azure_settings["userslistfilename"])
+        if os.path.exists(users_file):
+            with open(users_file, "r") as f:
+                content = f.read()
+                users_list = content.split("\n")
+        else:
+            print("Users list file: '{0}' does not exists.".format(azure_settings["userslistfilename"]))
+            raise FileNotFoundError
+    else:
+        users_list = sys.argv[1].split(",")
+    for user in users_list:
+        print(f"Getting messages for mailbox {user}: ")
+        message_page = graph.get_inbox(user)
+        if "error" in message_page.keys():
+            print(
+                "Error occurred to get messages for user {0}. {1}".format(
+                    user, message_page["error"]["message"]
+                )
+            )
+            break
+        for message in message_page["value"]:
+            print("Message:", message["subject"])
+            print("  From:", message["from"]["emailAddress"]["name"])
+            print("  Status:", "Read" if message["isRead"] else "Unread")
+            print("  Received:", message["receivedDateTime"])
+        # If @odata.nextLink is present
+        more_available = "@odata.nextLink" in message_page
+        print("\nMore messages available?", more_available, "\n")
